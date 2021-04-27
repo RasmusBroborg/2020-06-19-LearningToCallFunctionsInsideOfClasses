ea0f546c541d868335799420d73fe2ad939a94ac
RasmusBroborg <rasmus.broborg@icloud.com>
2021-04-27 15:58:02 +0200

    Bugfix

--- Binary-To-Decimals-Func.py
+++ Binary-To-Decimals-Func.py
@@ -6,3 +6,12 @@ def binaryToDecimals(binaryNum):
     x = 0
     y = length - 1
 
+
+    while x < length:
+        binaryDigit = int(str(binaryNum)[y]) * (2**x)
+        decimalsList += [binaryDigit]
+
+        y -= 1
+        x += 1
+
+    return sum(decimalsList)
