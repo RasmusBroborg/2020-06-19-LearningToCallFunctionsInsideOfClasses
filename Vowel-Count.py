2d723db651c685e55debc10ff3079619bac51f30
RasmusBroborg <rasmus.broborg@icloud.com>
2021-04-27 16:02:35 +0200

    Adding new file.

---
+++ Vowel-Count.py
@@ -0,0 +1,15 @@
+def vowelCounting():
+    x = input("Write a word or a sentence: ")
+    lenOfInput = len(x)
+    vowelCount = 0
+    vowelList = []
+    completeVowelList = ["a", "o", "u", "e", "i", "y"]
+
+    for i in range(0, lenOfInput):
+
+        if x[i] in completeVowelList:
+            vowelCount += 1
+            vowelList += x[i]
+
+    print("The user input " + x + " contains " + str(vowelCount) + " vowels. \n" +
+          "The list of vowels used are the following: " + str(vowelList))
