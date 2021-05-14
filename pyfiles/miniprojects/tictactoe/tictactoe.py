c6b05c212a39657723100508901061e4f08a19b6
RasmusBroborg <rasmus.broborg@icloud.com>
2021-05-14 15:02:54 +0200

    Commit before creating new branch.

--- pyfiles/miniprojects/tictactoe/tictactoe.py
+++ pyfiles/miniprojects/tictactoe/tictactoe.py
@@ -2,10 +2,18 @@ import pygame
 import sys
 
 # EACH SQUARE 100x100
-SCREENWIDTH = 100 * 3
-SCREENHEIGHT = 100 * 3
-# Update frequency
+# Width has two extra rows of pixels to compensate for line draws.
+# Height has extra 2 px for lines and extra 30 px for score bar at the top.
+#GAMEBAR_WIDTH = 100
+#GAMEBAR_HEIGHT = 30
+DISPLAY_WIDTH = 300
+DISPLAY_HEIGHT = 300  # + GAMEBAR_HEIGHT
+# Update frequency. Lower to decrease risk of double click
 FPS = 10
+
+COLOR_BLACK = (0, 0, 0)
+COLOR_GREY = (125, 125, 125)
+COLOR_WHITE = (255, 255, 255)
 
 
 """
@@ -17,9 +25,10 @@ Game:
     - Init zone as empty (None?)
     - Init player (start?)
 
-    - func to assign selected player
-
+    - func to assign selected players
         - event mouse click px zone add value to zone
+
+    - func to switch_player
         - if player x:
             player = o
         - if player o:
@@ -34,22 +43,95 @@ Game:
 
 """
 
+
+class Cell():
+    def __init__(self, x, y, width, height):
+        self.char_assignment = None
+        self.positional_info = (self.x)
+
+
+class Board:
+    def __init__(self):
+        self.lines = pygame.draw.aaline(
+            game_display, COLOR_WHITE, 0, 50)
+        self.player_o = "O"
+        self.player_x = "X"
+        self.player_x_score = 0
+        self.player_o_score = 0
+        self.round_count = 0
+        self.turn = 1
+        self.selected_player = player_x
+
+        self.cell_0 = Cell()
+        self.cell_1 = Cell()
+        self.cell_2 = Cell()
+
+        self.cell_3 = Cell()
+        self.cell_4 = Cell()
+        self.cell_5 = Cell()
+
+        self.cell_6 = Cell()
+        self.cell_7 = Cell()
+        self.cell_8 = Cell()
+
+        self.board_cells = [self.cell_0, self.cell_1, self.cell_2,
+                            self.cell_3, self.cell_4, self.cell_5,
+                            self.cell_6, self.cell_7, self.cell_8, ]
+
+    # DENNA MÅSTE SES ÖVER! SKA STARTA OM SPELET??
+
+    def player_win(self, selected_player):
+        if selected_player == self.player_o:
+            self.player_o_score += 1
+        elif selected_player == self.player_x:
+            self.player_x_score += 1
+
+    def draw(self):
+        pass
+        # if self.turn
+
+    def player_switch(self):
+        if self.selected_player == player_x:
+            self.selected_player = player_o
+        elif self.selected_player == player_o:
+            self.selected_player = player_x
+
+    def upd_turn_count(self):
+        self.turn += 1
+
+    def display_score(self):
+        pass
+
+    def game_logic():
+        pass
+
+
 def main():
+
+    game_display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGHT))
+    pygame.display.set_caption('Tic-Tac-Toe')
 
     reset_game = False
 
-    while not reset:
+    # Board init
+    board = Board
+
+    #basic_font = pygame.font.Font('freesansbold.ttf', 32)
+
+    while not reset_game:
 
         for event in pygame.event.get():
-            print(pygame.event)
+            print(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             # if event.type ==
-            #     reset_game = True
+            #
+
+        board.game_logic()
 
         pygame.display.update()
-        clock.tick(FPS)
+        pygame.time.Clock().tick(FPS)
 
 
 if __name__ == "__main__":
