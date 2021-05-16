7518a304e05d8ccfa6a3e338582ead8be540b3af
RasmusBroborg <rasmus.broborg@icloud.com>
2021-05-16 13:03:32 +0200

    Fixing bug related to player select in tictactoe.py. Writing logic for board square selection and assignment of player char in tictactoe.py. WIP fixing win-check logic in tictactoe.py.  Minor rewrite to ReadMe file.

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+The purpose of the repository is to have a place to test out coding ideas on a
+smaller scale.
+
+I quickly found that creating individual repositories for each coding idea
+resulted in a large quantity of small projects or unfinished projects that I was
+unable to progress on because of lack of knowledge.
+
+Instead of having a bloated number of repositories this repository contains mini
+projects, coding challenges, general ideas, and code from following tutorials.
--- pyfiles/miniprojects/tictactoe/tictactoe.py
+++ pyfiles/miniprojects/tictactoe/tictactoe.py
@@ -1,22 +1,128 @@
+"""
+
+Testing if I can create Tic-Tac-Toe in pygame using my current
+knowledge of python/pygame, without following a guide / other
+persons version of the game (no googling for Tic-Tac_Toe).
+
+"""
+
+
 import pygame
 import sys
+
+COLOR_BLACK = (0, 0, 0)
+COLOR_GREY = (125, 125, 125)
+COLOR_WHITE = (255, 255, 255)
+
+DISPLAY_WIDTH = 300
+DISPLAY_HEIGHT = 300
+FPS = 10
+
+
+def debug_board_status(board_list):
+    print(board_list)
 
 
 class Board:
     def __init__(self):
         self.board_list = [
+            ["X", "X", "X"],
             ["", "", ""],
-            ["", "X", ""],
             ["", "", ""]
         ]
+
+        # Corresponds to the board list. Marks top corner of each
+        # square on board.
+        self.board_display_pos = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ]
+
+        self.player_x = "X"
+        self.player_o = "O"
+        self.current_player = self.player_x
+
+        self.num_of_wins_player_x = 0
+        self.num_of_wins_player_o = 0
+
+        self.round = 0
+        self.turn = 1
+
+        self.sel_row = None
+        self.sel_column = None
 
     def check_board_availability(self, row, column):
 
         if self.board_list[row][column] != "":
             print("Not available. Contains: {}".format(
                 self.board_list[row][column]))
+            return False
+
         else:
             print("Available for selection.")
+            return True
+
+    def check_win(self):
+        b_list = self.board_list
+        player_winstring = self.current_player * 3
+
+        # Check row matches current players string:
+        if b_list[0][0] + b_list[0][1] + b_list[0][2] == player_winstring:
+            return True
+
+        # Check column win:
+
+        # Check diagonal win
+
+        if b_list[0] == 0:
+            return True
+
+        else:
+            return False
+
+    def next_turn(self):
+
+        print("Updating turn.")
+        # Switching players:
+        if self.current_player == self.player_x:
+            self.current_player = self.player_o
+        elif self.current_player == self.player_o:
+            self.current_player = self.player_x
+        elif self.current_player == None:
+            self.current_player = self.player_x
+
+        # Increment turn count:
+        self.turn += 1
+
+        # Inform player about turn:
+        print("Turn #{}. Player {} turn:".format(
+            self.turn, self.current_player))
+
+    def next_round(self):
+        # Resetting board
+        self.board_list = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ]
+
+        # Increment round and resetting turn count:
+        self.round += 1
+        self.turn = 1
+        self.current_player = self.player_x
+
+        print("Round {}.".format(
+            self.round))
+
+        print("Turn #{}. Player {} turn:".format(
+            self.turn, self.current_player))
+
+    def player_win(self, current_player):
+        if current_player == self.player_o:
+            num_of_wins_player_o += 1
+        if current_player == self.player_x:
+            num_of_wins_player_x += 1
 
 
 def draw_display(game_display):
@@ -41,7 +147,7 @@ def draw_display(game_display):
 def sel_square_position(m_pos):
     # Gets the list adress for the board squares based on the mouseclick pos.
     if m_pos != [[None], [None]]:
-        print("Sel x pos is {}, and sel y pos is {}.".format(
+        print("Mouse x pos is {}, and mouse y pos is {}.".format(
             m_pos[0], m_pos[1]))
 
         # Get which row in board list from mouse click y pos:
@@ -60,7 +166,7 @@ def sel_square_position(m_pos):
         elif m_pos[0] < 301:
             sel_pos_in_row = 2
 
-        print("Sel_row is {}, and sel_rowpos is {}.".format(
+        print("Based on mouse input, Row is {} and Column is {}.".format(
             sel_row, sel_pos_in_row))
 
         return sel_row, sel_pos_in_row
@@ -68,47 +174,52 @@ def sel_square_position(m_pos):
 
 def main():
 
-    DISPLAY_WIDTH = 300
-    DISPLAY_HEIGHT = 300
-    FPS = 10
-
-    COLOR_BLACK = (0, 0, 0)
-    COLOR_GREY = (125, 125, 125)
-    COLOR_WHITE = (255, 255, 255)
-
     game_display = pygame.display.set_mode((DISPLAY_WIDTH,
                                             DISPLAY_HEIGHT))
     pygame.display.set_caption('Tic-Tac-Toe')
 
     board = Board()
 
-    # basic_font = pygame.font.Font('freesansbold.ttf', 32)
+    board.next_round()
+
+    debug_board_status(board.board_list)
 
     while True:
         # Reset:
-        sel_row = None
-        sel_pos_in_row = None
 
         # Game start:
 
         for event in pygame.event.get():
-            print(event.type)
+
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-            # If mouse click on display surface:
+            # Checks if mouse click on display surface (pygame event code 1025):
             if event.type == 1025:
                 sel_square_pos = sel_square_position(pygame.mouse.get_pos())
 
-                print(sel_square_pos)
-                print(sel_square_pos[0])
-                print(sel_square_pos[1])
+                print("sel_square_pos contains value:".format(sel_square_pos))
 
-                board.check_board_availability(
+                square_availability = board.check_board_availability(
                     sel_square_pos[0], sel_square_pos[1])
 
-        # VISUALS
+                print("".format())
+
+                if square_availability == True:
+
+                    board.board_list[sel_square_pos[0]
+                                     ][sel_square_pos[1]] = board.current_player
+
+                    if board.check_win() == True:
+                        board.player_win()
+                        board.next_round()
+                    else:
+                        board.next_turn()
+
+                debug_board_status(board.board_list)
+
+                # VISUALS
 
         draw_display(game_display)
 
@@ -116,8 +227,6 @@ def main():
 
         pygame.display.update()
         pygame.time.Clock().tick(FPS)
-
-        # board.reset()
 
 
 if __name__ == "__main__":
