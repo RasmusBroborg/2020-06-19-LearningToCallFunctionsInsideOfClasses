901d348e254af6989d5d508db3aac2d4328dcc62
RasmusBroborg <rasmus.broborg@icloud.com>
2021-05-21 16:28:14 +0200

    Adding game logic and input handling in tictactoe_v02.py. Adding support for resolution scaling.

--- miniprojects/tictactoe/tictactoe_v02.py
+++ miniprojects/tictactoe/tictactoe_v02.py
@@ -1,245 +1,260 @@
 """
-
 Second attempt at creating Tic-Tac-Toe without following a guide or
 googling solutions .
+
+Testing the ability to run the game within a single class.
 
 """
 
 import pygame
 import sys
 
-pygame.init()
-
 class Tictactoe:
 
-    # Display setup and initialization
-
-    # Game variables initialization and calling of main method
     def __init__(self):
 
-        # Colors
+        pygame.init()
 
-        self.COLOR_BLACK = (0, 0, 0)
-        self.COLOR_WHITE = (255, 255, 255)
-
-        # Display constants
-
-        self.BOARD_WIDTH = 150 * 3
-        self.BOARD_HEIGHT = 150 * 3
-        self.MENU_BAR_WIDTH = self.BOARD_WIDTH
-        self.MENU_BAR_HEIGHT = int(self.BOARD_HEIGHT * 0.20)
-        self.DISPLAY_WIDTH = self.BOARD_WIDTH + self.MENU_BAR_WIDTH
-        self.DISPLAY_HEIGHT = self.BOARD_HEIGHT + self.MENU_BAR_HEIGHT
-        self.FPS = 10
-
-        self.game_display = pygame.display.set_mode((self.DISPLAY_WIDTH,
-                                                     self.DISPLAY_HEIGHT))
-        pygame.display.set_caption('Tic-Tac-Toe.')
-
-        # class variables
-
+        # Attributes related to game logic:
         self.board_list = [
             ["", "", ""],
             ["", "", ""],
             ["", "", ""]
         ]
 
-        self.turn = 0
         self.round = 0
-        self.available_players = ["X", "O"]
+        self.turn = 0
+
+        self.available_players = ("X", "O")
         self.current_player = "X"
-        self.selected_row = None
-        self.selected_column = None
-        self.playerX_wins = 0
+
         self.playerO_wins = 0
+        self.playerX_wins = 0
 
-        self.main()
+        # Attributes related to screen:
+        self.menu_bar_offset = 50
+        self.game_rectangle_length = 150
+        self.screen_width = self.game_rectangle_length * 3
+        self.screen_height = self.game_rectangle_length * 3 + self.menu_bar_offset
+        self.screen = pygame.display.set_mode((self.screen_width,
+                                               self.screen_height))
+        pygame.display.set_caption('Tic-Tac-Toe.')
 
-    def check_mouse_click(self, event_type):
-        if event_type == 1025:
-            return True
-        else:
-            return False
+        # Variables
+        self.mouse_pos = None
+        self.board_index = [None, None]
 
-    def convert_mouse_pos_to_board_index(self, m_pos):
-        # m_pos short for mouse x/y positions
+    def switch_player(self):
+        # Method
+        if self.current_player == self.available_players[0]:
+            self.current_player = self.available_players[1]
+        elif self.current_player == self.available_players[1]:
+            self.current_player = self.available_players[0]
+        print("Switching to player {}.".format(
+            self.current_player))
 
-        print("Mouse x pos is {}, and mouse y pos is {}.".format(
-            m_pos[0], m_pos[1]))
+    def next_turn(self):
+        print("\nUpdating to next turn.")
+        self.turn += 1
+        print("""Incrementing turn count to {}.""".format(
+            self.turn))
 
-        # Get which row in board list from mouse click y pos:
-        if m_pos[1] < 100 and m_pos[1] >= 0:
-            row = 0
-        elif m_pos[1] < 200:
-            row = 1
-        elif m_pos[1] < 301:
-            row = 2
-
-        # Get which position in row from mouse click x pos:
-        if m_pos[0] < 100 and m_pos[1] >= 0:
-            column = 0
-        elif m_pos[0] < 200:
-            column = 1
-        elif m_pos[0] < 301:
-            column = 2
-
-        print("Based on mouse input, Row is {} and Column is {}.".format(
-            row, column))
-
-        self.selected_column = column
-        self.selected_row = row
-
-    def check_board_availability(self):
-        if self.board_list[self.selected_row][self.selected_column] != "":
-            return False
-        else:
-            return True
-
-    def assign_current_player_to_board(self):
-        self.board_list[self.selected_row][selected_column] = self.current_player
-
-    def check_if_win(self):
-
-        def get_winning_combination(board, sel_player):
-            win_combo = []
-            for i in range(len(board)):
-                win_combo.append(sel_player)
-            return win_combo
-
-        def check_rows_for_win(board, winning_combo):
-            for i in range(len(board)):
-                if board[i] == winning_combo:
-                    return True
-
-        def check_columns_for_win(board, winning_combo):
-            for i in range(len(board)):
-                column = [board[0][i]] + \
-                    [board[1][i]] + [board[2][i]]
-                if column == winning_combo:
-                    print("Vertical win.")
-                    return True
-
-        def check_diagonals_for_win(board, winning_combo):
-            # Diagonal top to bottom
-            diagonal_top_to_bottom = []
-            for i in range(len(board)):
-                diagonal_top_to_bottom.insert(i, board[i][i])
-
-            # Diagonal bottom to top
-            diagonal_bottom_to_top = []
-            for i in range(len(board)):
-                board_index = (len(board) - i - 1)
-                diagonal_bottom_to_top.insert(
-                    i, board[i][board_index])
-
-            if diagonal_top_to_bottom == winning_combo or diagonal_bottom_to_top == winning_combo:
-                print("Diagonal win.")
-                return True
-            else:
-                return False
-
-        winning_combination = get_winning_combination(
-            self.board_list, self.current_player)
-        row_win = check_rows_for_win(self.board_list, winning_combination)
-        column_win = check_columns_for_win(
-            self.board_list, winning_combination)
-        diagonal_win = check_diagonals_for_win(
-            self.board_list, winning_combination)
-
-        if row_win == True or column_win == True or diagonal_win == True:
-            return True
-
-    def add_win_to_current_player(self):
-
-        for player in self.available_players:
-            if self.current_player == "X":
-                self.playerX_wins += 1
-            elif self.current_player == "Y":
-                self.playerX_wins += 1
-
-    def update_next_round(self):
+    def next_round(self):
+        self.board_list = [
+            ["", "", ""],
+            ["", "", ""],
+            ["", "", ""]
+        ]
         self.round += 1
         self.turn = 1
+        print("""\n
+Resetting board to empty string values.
+Incrementing round count to {}. Resetting turn count to {}.\n""".format(self.round, self.turn))
 
-    def update_next_turn(self):
-        self.turn += 1
+    #'Check if' methods returns True/Fals
 
-    def draw_display(self):
+    def check_if_win(self):
+        print("Checking if move resulted in win in nested functions:")
+        ref_win_list = []
 
-        font = pygame.font.Font('freesansbold.ttf', 32)
+        # Nested methods returns True if win is found:
+        def append_win_combination_to_win_list():
+            for i in range(len(self.board_list)):
+                ref_win_list.append(self.current_player)
+            print("\n     Winning combination is {}.".format(ref_win_list))
 
-        self.game_display.fill(self.COLOR_BLACK)
+        def check_rows_and_columns_for_win():
+            print("\n     Checking rows and columns for win")
+            for first_index in range(len(self.board_list)):
 
-        # Drawing vertical lines:
-        pygame.draw.aaline(self.game_display, self.COLOR_WHITE,
-                           (int(self.BOARD_WIDTH * 0.33), 0),
-                           (int(self.BOARD_WIDTH * 0.33), self.BOARD_HEIGHT))
-        pygame.draw.aaline(self.game_display, self.COLOR_WHITE,
-                           (int(self.BOARD_WIDTH * 0.66), 0),
-                           (int(self.BOARD_WIDTH * 0.66), self.BOARD_HEIGHT))
-        # Drawing horizontal lines:
-        pygame.draw.aaline(self.game_display, self.COLOR_WHITE,
-                           (0, int(self.BOARD_HEIGHT * 0.33)),
-                           (self.BOARD_WIDTH, int(self.BOARD_HEIGHT * 0.33)))
-        pygame.draw.aaline(self.game_display, self.COLOR_WHITE,
-                           (0, int(self.BOARD_HEIGHT * 0.66)),
-                           (self.BOARD_WIDTH, int(self.BOARD_HEIGHT * 0.66)))
+                row_list = []
+                column_list = []
 
-        # Iterates through the board_list and draws the symbols on the visual
-        # board:
+                for second_index in range(len(self.board_list)):
+                    row_list.append(self.board_list[first_index][second_index])
+                    column_list.append(
+                        self.board_list[second_index][first_index])
 
-        for row_index in range(len(self.board_list)):
-            for column_index in range(len(self.board_list[0])):
-                symbol = self.board_list[row_index][column_index]
+                    if len(row_list) == len(ref_win_list):
+                        print(
+                            "     - Row #{} contains: {}".format(first_index, row_list))
+                        print(
+                            "     - Column #{} contains: {}".format(first_index, column_list))
+                    if row_list == ref_win_list or column_list == ref_win_list:
+                        print("     - Winning combination found. Returning True.")
+                        return True
+            print(
+                "     No winning combinations found in rows or columns. Returning False.")
+            return False
 
-                # Magic numbers represent the pixel offset
-                x_pos = column_index * 100 + 50
-                y_pos = row_index * 100 + 50
+        def check_diagonals_for_win():
+            print("\n     Checking diagonals for win:")
 
-                symbol_text = font.render(
-                    symbol, True, self.COLOR_WHITE, self.COLOR_BLACK)
-                textRect = symbol_text.get_rect()
-                textRect.center = (x_pos, y_pos)
+            diagonal_top_bot = []
+            diagonal_bot_top = []
 
-                self.game_display.blit(symbol_text, textRect)
+            for index in range(len(self.board_list)):
+                diagonal_top_bot.append(
+                    self.board_list[index][index])
 
-    def main(self):
+                # -1 to compensate for index offset
+                diagonal_bot_top.append(
+                    self.board_list[len(self.board_list) - index - 1][index])
+
+            print("     - Diagonal top to bot contains: {}.".format(diagonal_top_bot))
+            print("     - Diagonal bot to top contains: {}.".format(diagonal_bot_top))
+
+            if diagonal_top_bot == ref_win_list or diagonal_bot_top == ref_win_list:
+                print("     - Winning combination found in diagonals. Returning True.")
+                return True
+            else:
+                print(
+                    "     No winning combinations found in diagonals. Returning False.\n")
+                return False
+
+        append_win_combination_to_win_list()
+        if check_rows_and_columns_for_win() == True or check_diagonals_for_win() == True:
+            print("     Nested functions found win. Parent function returns True.")
+            return True
+
+        print("     No wins found in nested functions. Parent function returns False.")
+        return False
+
+    def check_if_draw(self):
+        print("Checking if draw...")
+        if self.turn >= 9:
+            print("It's a draw! Returning True.")
+            return True
+        print("No draw. Returning False.")
+        return False
+
+    def player_win(self):
+        if self.current_player == self.available_players[0]:
+            self.playerX_wins += 1
+        elif self.current_player == self.available_players[1]:
+            self.playerO_wins += 1
+
+        print("""Player {} wins the round! Incrementing player win score.""".format(
+            self.current_player))
+
+    def check_if_mouseclick_on_board(self, event_type):
+        if event_type == 1025:
+            print("Checking if mouseclick is within the board bounds.")
+            self.mouse_pos = pygame.mouse.get_pos()
+            print("Mouseclick detected at x pos {}, and y pos {}.".format(
+                self.mouse_pos[0], self.mouse_pos[1]))
+
+            if self.mouse_pos[1] < self.menu_bar_offset:
+                print("Position is outside of board bounds. Unvalid selection.\n")
+                return False
+            else:
+                print("Position is within the board bounds. Returning True.")
+                return True
+        return False
+
+    def convert_mouse_pos_to_board_index(self):
+        print("Converting mouse pos to board index.")
+
+        def assign_row_index():
+            for i in range(len(self.board_list)):
+                if self.mouse_pos[1] - self.menu_bar_offset <= self.game_rectangle_length * (i + 1):
+                    print("Mouse y pos {} corresponds to row index {}.".format(
+                        self.mouse_pos[1], i))
+                    self.board_index[0] = i
+                    break
+
+        def assign_column_index():
+            for i in range(len(self.board_list)):
+                if self.mouse_pos[0] <= self.game_rectangle_length * (i + 1):
+                    print("Mouse y pos {} corresponds to column index {}.".format(
+                        self.mouse_pos[0], i))
+                    self.board_index[1] = i
+                    break
+
+        assign_row_index()
+        assign_column_index()
+
+        print("Selected board index is now set to {}.".format(self.board_index))
+
+    def check_if_board_index_is_available(self):
+        print("Checking if board index is available or contains value...")
+        if self.board_list[self.board_index[0]][self.board_index[1]] == "":
+            print("Board position is available. Returning True.")
+            return True
+        else:
+            print("Position is unavailable. Contains {}.".format(
+                self.board_list[self.board_index[0]][self.board_index[1]]))
+            return False
+
+    def assign_current_player_to_board_index(self):
+        self.board_list[self.board_index[0]
+                        ][self.board_index[1]] = self.current_player
+        print("Assigning player {} to board_list[{}][{}].".format(
+            self.current_player, self.board_index[0], self.board_index[1]))
+
+    def run_game(self):
+        pygame.init()
+        game_display = pygame.display.set_mode((self.screen_width,
+                                                self.screen_height))
+        pygame.display.set_caption('Tic-Tac-Toe.')
+
+        # Starting first round:
+        self.next_round()
 
         while True:
-
             # Game start:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
 
-                # Game logic step by step:
-                if self.check_mouse_click(event) == True:
+                if self.check_if_mouseclick_on_board(event.type) == True:
                     self.convert_mouse_pos_to_board_index()
 
-                    if self.check_board_availability() == True:
-                        self.assign_current_player_to_board()
+                    if self.check_if_board_index_is_available() == True:
+                        self.assign_current_player_to_board_index()
 
                         if self.check_if_win() == True:
-                            self.add_win_to_current_player()
-                            self.update_next_round()
+                            self.player_win()
+                            self.next_round()
+                            self.switch_player()
 
-                        elif self.turn >= 9:
-                            self.update_next_round()
+                        elif self.check_if_draw() == True:
+                            self.next_round()
+                            self.switch_player()
 
                         else:
-                            self.update_next_turn()
+                            self.next_turn()
+                            self.switch_player()
 
-                    debug_board(board.board_list)
-
-            self.draw_display()
-# VISUALS
-
-            #draw_display(game_display, board.board_list)
+            # pygame.display.flip()
 
             pygame.display.update()
-            pygame.time.Clock().tick(self.FPS)
+            pygame.time.Clock().tick(10)
 
 
 if __name__ == "__main__":
-    Tictactoe()
+
+    tictactoe = Tictactoe()
+
+    tictactoe.run_game()
