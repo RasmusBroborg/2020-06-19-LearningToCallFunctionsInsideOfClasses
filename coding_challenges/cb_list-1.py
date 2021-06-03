dd73ae22aa06a9eee210d0c88c3448b99c8462ea
RasmusBroborg <rasmus.broborg@icloud.com>
2021-06-03 12:21:53 +0200

    Adding last addition to cb_list-1.py. Touch cb_logic-1.py along with 8 additions.

--- coding_challenges/cb_list-1.py
+++ coding_challenges/cb_list-1.py
@@ -146,4 +146,52 @@ sum2([1, 1, 1, 1]) → 2
 
 def sum2(nums):
     if len(nums) == 1:
-        return nums[0]# EJ FÄRDIG
+        return nums[0]  # EJ FÄRDIG
+
+
+"""
+https://codingbat.com/prob/p171011
+Given 2 int arrays, a and b, each length 3, return a new array length 2
+containing their middle elements.
+
+middle_way([1, 2, 3], [4, 5, 6]) → [2, 5]
+middle_way([7, 7, 7], [3, 8, 0]) → [7, 8]
+middle_way([5, 2, 9], [1, 4, 5]) → [2, 4]
+"""
+
+
+def middle_way(a, b):
+    return [a[1], b[1]]
+
+
+"""
+https://codingbat.com/prob/p124806
+Given an array of ints, return a new array length 2 containing the first and
+last elements from the original array. The original array will be length 1 or
+more.
+
+make_ends([1, 2, 3]) → [1, 3]
+make_ends([1, 2, 3, 4]) → [1, 4]
+make_ends([7, 4, 6, 2]) → [7, 2]
+"""
+
+
+def make_ends(nums):
+    return [nums[0], nums[-1]]
+
+
+"""
+https://codingbat.com/prob/p177892
+Given an int array length 2, return True if it contains a 2 or a 3.
+
+has23([2, 5]) → True
+has23([4, 3]) → True
+has23([4, 5]) → False
+"""
+
+
+def has23(nums):
+    if 2 in nums or 3 in nums:
+        return True
+    else:
+        return False
